87f7c079b1e8e38df46bf39edaa7fe65b7f3c0c9
Alan Poveda <alsepoveda@gmail.com>
2022-03-16 15:03:50 -0300

    Add the data to get the option

--- ImageBanner/digisystemdev/sp_widget/DGS Image Banner Onboarding/4 Server Script.js
+++ ImageBanner/digisystemdev/sp_widget/DGS Image Banner Onboarding/4 Server Script.js
@@ -2,6 +2,6 @@
   /* populate the 'data' object */
   /* e.g., data.table = $sp.getValue('table'); */
 	data.image = options.image_url;
-	
-
+	data.width = options.width;
+  data.height = options.height;
 })();
