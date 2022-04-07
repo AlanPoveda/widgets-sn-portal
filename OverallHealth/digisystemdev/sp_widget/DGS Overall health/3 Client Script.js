3eac3d36fb9cd90168c8c6714080242faef60498
Alan Poveda <alsepoveda@gmail.com>
2022-04-07 16:29:25 -0300

    OverallHealth frirst version

--- OverallHealth/digisystemdev/sp_widget/DGS Overall health/3 Client Script.js
+++ OverallHealth/digisystemdev/sp_widget/DGS Overall health/3 Client Script.js
@@ -1,1 +1,14 @@
-api.controller=function() {  /* widget controller */  var c = this;};
+api.controller=function($rootScope) {
+  /* widget controller */
+  var c = this;
+  
+  
+	//Comunicação entre um widget quando troca o projeto
+  $rootScope.$on('selected.project', function(evt, data){
+		c.server.update().then(function(){
+      console.log(c.data.style);
+      console.log(c.data.overall_status);
+    });
+	});
+
+};
